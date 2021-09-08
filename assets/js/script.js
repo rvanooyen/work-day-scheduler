4e13c4720caa8ccd102e26b1ff7a7e510e882dc9
rvanooyen <robert.vanooyen@gmail.com>
2021-09-07 21:39:40 -0500

    update

---
+++ assets/js/script.js
@@ -0,0 +1,100 @@
+var currentDayEl = $("#currentDay");
+
+var getCurrentDate = function() {
+    var today = moment().format("dddd MMM Do");
+    console.log(today);
+    currentDayEl.val = today;
+    currentDayEl.appendChild(today);
+            
+};
+
+getCurrentDate();
+
+// // task text was clicked
+// $(".list-group").on("click", "p", function() {
+//     // get current text of p element
+//     var text = $(this)
+//       .text()
+//       .trim();
+  
+//     // replace p element with a new textarea
+//     var textInput = $("<textarea>").addClass("form-control").val(text);
+//     $(this).replaceWith(textInput);
+  
+//     // auto focus new element
+//     textInput.trigger("focus");
+//   });
+
+// var auditTask = function(taskEl) {
+//     // get date from task element
+//     var date = $(taskEl)
+//       .find("span")
+//       .text()
+//       .trim();
+  
+//     // convert to moment object at 5:00pm
+//     var time = moment(date, "L").set("hour", 17);
+  
+//     // remove any old classes from element
+//     $(taskEl).removeClass("list-group-item-warning list-group-item-danger");
+  
+//     // apply new class if task is near/over due date
+//     if (moment().isAfter(time)) {
+//       $(taskEl).addClass("list-group-item-danger");
+//     } else if (Math.abs(moment().diff(time, "days")) <= 2) {
+//       $(taskEl).addClass("list-group-item-warning");
+//     }
+//   };
+
+// var saveTasks = function() {
+//     localStorage.setItem("tasks", JSON.stringify(tasks));
+//   };
+
+// var createTask = function(taskText, taskDate, taskList) {
+//     // create elements that make up a task item
+//     var taskLi = $("<li>").addClass("list-group-item");
+//     var taskSpan = $("<span>")
+//       .addClass("badge badge-primary badge-pill")
+//       .text(taskDate);
+//     var taskP = $("<p>")
+//       .addClass("m-1")
+//       .text(taskText);
+  
+//     // append span and p element to parent li
+//     taskLi.append(taskSpan, taskP);
+  
+//     // check due date
+//     auditTask(taskLi);
+  
+//     // append to ul list on the page
+//     $("#list-" + taskList).append(taskLi);
+//   };
+  
+//   var loadTasks = function() {
+//     tasks = JSON.parse(localStorage.getItem("tasks"));
+  
+//     // if nothing in localStorage, create a new object to track all task status arrays
+//     if (!tasks) {
+//       tasks = {
+//         toDo: [],
+//         inProgress: [],
+//         inReview: [],
+//         done: []
+//       };
+//     }
+  
+//     // loop over object properties
+//     $.each(tasks, function(list, arr) {
+//       // then loop over sub-array
+//       arr.forEach(function(task) {
+//         createTask(task.text, task.date, list);
+//       });
+//     });
+//   };
+  
+//   var saveTasks = function() {
+//     localStorage.setItem("tasks", JSON.stringify(tasks));
+//   };
+
+// // convert to moment object at 5:00pm
+// var time = moment(date, "L").set("hour", 17);
